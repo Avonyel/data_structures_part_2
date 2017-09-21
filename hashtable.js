e3b26eba5a041263e6a514cd62d9036224161a4c
Stephanie Barker <song_mage@yahoo.com>
2017-09-21 14:54:44 -0400

    got some rudimentary balancing working

--- hashtable.js
+++ hashtable.js
@@ -2,20 +2,35 @@ const LinkedList = require("./linkedlist");
 const dictionary = require("./dictionary.json");
 
 class HashTable {
-  constructor() {
-    this.buckets = [];
+  constructor(tuning = 50, buckets = 50) {
+    this.buckets = new Array(buckets);
+    this.tuning = tuning;
+    this.entries = 0;
+  }
+
+  balance() {
+    if (!(this.entries / this.buckets.length > this.tuning)) return;
+    const currentBuckets = this.renderList();
+    this.buckets = new Array(this.buckets.length * 2);
+
+    currentBuckets.forEach(bucket => {
+      this.insert(bucket[0], bucket[1]);
+    });
+
+    console.log("Balancing: ", this.buckets.length, " buckets");
   }
 
   hash(input) {
-    let first = input.charCodeAt(0);
-    return (first -= 97);
+    return (
+      input.split("").reduce((sum, letter) => {
+        return sum + letter.charCodeAt(0) - 64;
+      }, 0) % this.buckets.length
+    );
   }
 
   insert(word, definition) {
     if (typeof word[0] !== "string") return console.log("strings only");
     word = word.toLowerCase();
-    if (word.charCodeAt(0) < 97 || word.charCodeAt(0) > 122)
-      return console.log("no numbers or special chars allowed", word);
 
     const index = this.hash(word);
 
@@ -24,23 +39,28 @@ class HashTable {
     } else {
       this.buckets[index] = new LinkedList([[word, definition]]);
     }
+    this.entries++;
+    this.balance();
   }
 
   renderList() {
     let counter = 0;
+    let giantArray = [];
 
     for (let i = 0; i < this.buckets.length; i++) {
       counter = 0;
       if (this.buckets[i]) {
         let node = this.buckets[i].readNode(counter);
-        console.log(node.data);
+        giantArray.push(node.data);
         while (node.nextNode) {
           counter++;
           node = this.buckets[i].readNode(counter);
-          console.log(node.data);
+          giantArray.push(node.data);
         }
       }
     }
+
+    return giantArray;
   }
 
   define(word) {
@@ -60,7 +80,7 @@ class HashTable {
   }
 }
 
-const testHash = new HashTable(26);
+const testHash = new HashTable();
 
 // testHash.insert("Dalphabet", "an alphabet starting with D");
 //
